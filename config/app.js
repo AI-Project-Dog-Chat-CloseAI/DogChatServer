c51d825b6c1184d8207967864049e9d95ae44b48
YukiElectric <phamthanhlap2003.hha@gmail.com>
2024-03-25 09:48:52 +0700

    makrov chain alth in chatbot

--- config/app.js
+++ config/app.js
@@ -2,5 +2,6 @@ module.exports = {
     port : process.env.SERVER_PORT || 7200,
     view_folder : `${__dirname}/../src/apps/views`,
     view_engine : "ejs",
-    key : process.env.KEY
+    key : process.env.KEY,
+    client_domain : process.env.DOMAIN
 }
--- src/apps/app.js
+++ src/apps/app.js
@@ -11,9 +11,12 @@ const { Server } = require('socket.io');
 const io = new Server(server);
 const db = require('../common/database');
 const jwt = require('jsonwebtoken');
+const chat = require('../common/dogchat');
 
 app.set("views", config.get("app.view_folder"));
 app.set("view engine", config.get("app.view_engine"));
+
+var users = JSON.parse(db.readDb());
 
 io.on('connection', (socket) => {
     console.log(`Client ${socket.id} connected from ${socket.handshake.address}`);
@@ -22,7 +25,6 @@ io.on('connection', (socket) => {
     });
 
     socket.on('login', (data) => {
-        var users = JSON.parse(db.readDb());
         var existing = false;
         users.map((item) => {
             if (item.email === data.email && item.active) existing = true;
@@ -31,28 +33,32 @@ io.on('connection', (socket) => {
         else socket.emit('login', { status: false, message: "Email or password is incorrect" });
     })
 
-    socket.on('register', async (data) => {
-        var users = JSON.parse(db.readDb());
+    socket.on('register',async (data) => {
         var existing = false;
         users.map((item) => {
             if (item.email === data.email) existing = true;
         });
         if (existing) socket.emit('register', { status: false, message: "Email is existing" });
         else {
+            socket.emit('register', { status: true, message: "Create account successfully" });
             const viewPath = app.get("views");
             const token = jwt.sign({ email: data.email }, config.get('app.key'));
-            const html = await ejs.renderFile(path.join(viewPath, "verify.ejs"), { email: data.email, token });
+            const html = await ejs.renderFile(path.join(viewPath, "verify.ejs"), { email: data.email, token, domain: config.get('app.client_domain') });
             await transporter(html, data.email);
             users.push({ ...data, active: false });
             db.writeDb(users);
-            socket.emit('register', { status: true, message: "Create account successfully" });
+            console.log(`Send token to ${data.email} successfully`);
         }
     })
 
+    // socket.on('message', (data) => {
+    //     setTimeout(() => {
+    //         socket.emit('message', { status: true, message: "天気がいいから、散歩しましょう！！！！" });
+    //     }, 5000);
+    // })
+
     socket.on('message', (data) => {
-        setTimeout(() => {
-            socket.emit('message', { status: true, message: "天気がいいから、散歩しましょう！！！！" });
-        }, 5000);
+        socket.emit('message', { status: true, message: chat(data.message) });
     })
 
     socket.on('active', (data) => {
@@ -60,14 +66,15 @@ io.on('connection', (socket) => {
             if (err) {
                 socket.emit('active', { status: false, message: "Can't active your account." });
             } else {
-                var users = JSON.parse(db.readDb());
                 const email = data.email;
                 users.map((item) => {
                     if (item.email === email) item.active = true;
                     return item;
                 });
                 db.writeDb(users);
-                socket.emit('active', { status: true, message: "Active your account successfully." });
+                setTimeout(() => {
+                    socket.emit('active', { status: true, message: "Active your account successfully." })
+                }, 3000);
             }
         })
     })
